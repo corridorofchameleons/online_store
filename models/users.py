aaae009bdc09855c17810b97c1f8914d0a09fb95
Master Chief <gdramze@gmail.com>
2024-09-03 23:03:03 +0300

    add alembic and users table

---
+++ models/users.py
@@ -0,0 +1,19 @@
+from _datetime import datetime, UTC
+
+from sqlalchemy import MetaData, Table, Column, Integer, String, TIMESTAMP, ForeignKey
+
+metadata = MetaData()
+
+
+users = Table(
+    'users',
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('name', String, nullable=False),
+    Column('email', String, nullable=False),
+    Column('phone', String, nullable=False),
+    Column('password', String, nullable=False),
+    Column('registered_at', TIMESTAMP, default=datetime.now(UTC))
+)
+
+
