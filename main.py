ebd2294aa94dc2ff83e6aac84ec21ad8cbc329f2
Master Chief <gdramze@gmail.com>
2024-09-15 21:20:54 +0300

    add docker

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from routers.users import router as users_router
 from routers.store import router as store_router
 
@@ -7,3 +7,13 @@ app = FastAPI()
 
 app.include_router(users_router, prefix='/users')
 app.include_router(store_router, prefix='/store')
+
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
